7a26442b155f50e5d9e29f699b240ab74a45a4be
shotamishima <sm4mma@gmail.com>
2022-04-17 20:41:56 +0900

    Modified.

--- structure.cpp
+++ structure.cpp
@@ -6,11 +6,28 @@ using namespace std;
 int main() {
     string str;
 
+    // do while
+    cout << "--test do while--" << endl;
     do {
         cout << "Enter text: ";
         getline(cin, str);
         cout << "You enterd: " << str << endl;
     } while (str != "goodbye");
+
+    // for loop
+    cout << "--test for loop--" << endl;
+    for (int n = 10; n > 0; n--) {
+        cout << n << ", ";
+    }
+    cout << "liftoff!" << endl;
+    
+    // range based for loop
+    cout << "--test for loop (range-based)--" << endl;
+    str = {"Hello!"};
+    for (char c: str) {
+        cout << "[" << c << "]";
+    }
+    cout << endl;
 
     return 0;
 }
